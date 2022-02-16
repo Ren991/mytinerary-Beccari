1a9e60f073165717cce6d6043f81d7f4dde3e3cc
Ren991 <beccrenzo@gmail.com>
2022-02-16 16:34:19 -0300

    agregue un h1

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  <h1>hola</h1>
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
